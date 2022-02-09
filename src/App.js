fabdb321224b3d69fc37612ac35c4cd784fb279e
Andrew <andrew.morgan@e.org>
2022-02-09 17:29:09 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   
-  async function fetchMoviesHandler(){
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -37,7 +37,11 @@ function App() {
       setError(error.message);
     }; 
     setIsLoading(false);   
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
